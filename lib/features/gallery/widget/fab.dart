0b2bcc700798b3918b85fe97174edcc4750016ef
BashCooler <den.tryaskin@mail.ru>
2026-07-07 20:37:08 +0500

    Hide fab on transition

--- lib/features/gallery/widget/fab.dart
+++ lib/features/gallery/widget/fab.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:hydit/features/gallery/getx/gallery.dart';
 
 import 'package:hydit/widgets/switcher.dart';
 import 'package:hydit/widgets/acrylic.dart' as a;
@@ -17,13 +18,14 @@ class GalleryFAB extends StatelessWidget {
   const GalleryFAB({super.key, required this.tag});
 
   FileStore get files => Get.find(tag: tag);
+  GalleryController get gallery => Get.find(tag: tag);
   SelectionController get selection => Get.find(tag: tag);
 
   @override
   Widget build(BuildContext context) {
     return Obx(() {
       return SequencedSwitcher(
-        visible: selection.off,
+        visible: selection.off && gallery.badges,
         showFirst: files.loader!.failed,
         first: FAB(
           onPressed: files.loader!.retry,
