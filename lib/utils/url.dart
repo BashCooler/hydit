9ce1e47284aa70265b95c16c344b706a38d2715d
BashCooler <den.tryaskin@mail.ru>
2026-09-17 22:29:24 +0500

    Update url.dart

--- lib/utils/url.dart
+++ lib/utils/url.dart
@@ -1,33 +1,56 @@
 import 'package:hydit/services/executor.dart';
 
 
+enum UrlErrorCode {
+  protocolEmpty,
+  urlInvalid,
+  portInvalid,
+  hostEmpty,
+  pathNotEmpty,
+}
+
+
+extension ToFailure on UrlErrorCode {
+  Failure<T> toFailure<T>() => Failure(
+    switch (this) {
+      .pathNotEmpty => 'Unsupported',
+      _ => 'Input error',
+    },
+    switch (this) {
+      .protocolEmpty => 'URL must start with "http://" or "https://"',
+      .urlInvalid => 'Invalid URL',
+      .portInvalid => 'Invalid port',
+      .hostEmpty => 'Host is empty',
+      .pathNotEmpty => 'URL path must be empty',
+    },
+  );
+}
+
+
 Result<Uri> parseUrl(String url) {
   final uri = Uri.tryParse(url);
 
   final str = uri.toString();
 
   if (!str.startsWith('http://') && !str.startsWith('https://')) {
-    return Failure(
-      'Input error',
-      'URL must start with "http://" or "https://"',
-    );
+    return UrlErrorCode.protocolEmpty.toFailure();
   }
 
   if (uri == null) {
-    return Failure('Input error', 'Invalid URL');
+    return UrlErrorCode.urlInvalid.toFailure();
   }
 
   if (uri.hasPort && (uri.port > 65535 || uri.port < 0)) {
-    return Failure('Input error', 'Invalid port');
+    return UrlErrorCode.portInvalid.toFailure();
   }
 
   if (uri.host.isEmpty) {
-    return Failure('Input error', 'Host is empty');
+    return UrlErrorCode.hostEmpty.toFailure();
   }
 
   if (uri.path.isNotEmpty && uri.path != '/') {
-    return Failure('Unsupported', 'URL path must be empty');
+    return UrlErrorCode.pathNotEmpty.toFailure();
   }
 
-  return Success(uri);
+  return uri.toSuccess();
 }
